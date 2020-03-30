681b1b6e71f2f101c45c35a5a462d53c346b4119
adam-pawelek <adam.pwk@outlook.com>
2020-03-30 20:38:33 +0200

    Prawie skonczone Line, Point (nie testowane)

--- laby1/prezentacja1/src/Line.java
+++ laby1/prezentacja1/src/Line.java
@@ -1,4 +1,37 @@
 public class Line {
-    //int x;
-    //int y;
+    Point p1;
+    Point p2;
+
+    public Line(int x1, int y1,int x2, int y2){
+        this.p1 = new Point(x1,y1);
+        this.p2 = new Point(x2,y2);
+    }
+
+
+    public void move(int x, int y){  // przesuniecie o wektor
+        this.p1.move(x,y);
+        this.p2.move(x,y);
+    }
+    public void flip(){
+        this.p1.flip();
+        this.p2.flip();
+    }
+
+    public void wypisz(){
+        System.out.println("punk 1 to" );
+    }
+
+    public String toString(){
+        String wynik = "Punkt 1 \n";
+        wynik += p1.toString();
+        wynik +="\n";
+        wynik += "Punkt 2 \n";
+        wynik += p2.toString();
+        return wynik;
+    }
+
+
+
+
+
 }
---
+++ laby1/prezentacja1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("asdfasd");
+        Line linia = new Line(4,5,4,5);
+        System.out.println(linia.toString());
+    }
+}
+
